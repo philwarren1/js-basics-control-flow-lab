1618257331215d4aa5c9d06b3d5f637dc0e5c674
Philip <philwarren1@gmail.com>
2021-04-07 10:28:24 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response;
+
+  if (distance <= 400) {
+    response = 'This one is on me!';
+  } else if (distance >= 2000 && distance < 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else {
+    response = 'No can do.';
+  }
+  return  response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+}
 }
